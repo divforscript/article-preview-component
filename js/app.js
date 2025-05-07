ba7328f9346c03ab60dda2a67e5f1bec74bad301
Dayvid Ramos <d.ramosm97@gmail.com>
2025-05-07 01:31:00 -0500

    fixed: share-icon functions
    description:
      * share-icon func: Show and hide the share-bar
        with FB, X and Pinterest links (icons).
    
        Allows to user close the share-bar clicked on
        anywhere of screen.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,38 @@
 const shareSection = document.getElementById("share-section")
 const forwardArrowWrapper1 = document.getElementById("forward-arrow-wrapper1")
 const forwardArrowWrapper2 = document.getElementById("forward-arrow-wrapper2")
-
+const insetBackground = document.getElementById("inset-background")
 
 console.log(forwardArrowWrapper1)
 let isShareBarShowed = false
 
 
-window.onresize = () => {
-  isShareBarShowed = false
-  shareSection.classList.add("hidden")
-}
 
 
 forwardArrowWrapper1.onclick = () => {
   isShareBarShowed = !isShareBarShowed
 
   if(isShareBarShowed) {
+    console.log("sí, de acuerdo")
     shareSection.classList.remove("hidden")
+    insetBackground.classList.remove("hidden")
     return
   }
 
   shareSection.classList.add("hidden")
 }
 
+
+insetBackground.onclick = () => {
+  isShareBarShowed = false
+  shareSection.classList.add("hidden")
+  insetBackground.classList.add("hidden")
+}
+
 forwardArrowWrapper2.onclick = () => {
 
   isShareBarShowed = false
   shareSection.classList.add("hidden")
+  insetBackground.classList.add("hidden")
+  
 }
